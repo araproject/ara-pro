1f7a4072f3370f90da4b06bc8c52f5a70044d4eb
araproject <55191192+araproject@users.noreply.github.com>
2019-09-25 19:15:33 +0800

    Update index.js

--- src/layout/index.js
+++ src/layout/index.js
@@ -6,6 +6,7 @@ import Footer from '../components/Footer'
 import config from '../../data/SiteConfig'
 import favicon from '../images/favicon1.png'
 import '../styles/main.scss'
+import AdSense from 'react-adsense'
 
 export default class MainLayout extends Component {
   static contextType = ThemeContext
@@ -30,6 +31,14 @@ export default class MainLayout extends Component {
         >
           <meta name="description" content={config.siteDescription} />
           <link rel="shortcut icon" type="image/png" href={favicon} />
+<AdSense.Google
+  client='ca-pub-5908100338136939'
+  slot='2481741157'
+  style={{ display: 'block' }}
+  format='auto'
+  responsive='true'
+  layoutKey='-gw-1+2a-9x+5c'
+/>
         </Helmet>
         <Navigation menuLinks={config.menuLinks} />
         <main id="main-content">{children}</main>
