6a2901cb452495a747bb9e4c41b1a7604222d5cb
araproject <55191192+araproject@users.noreply.github.com>
2019-09-27 13:28:59 +0800

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,17 +1,17 @@
 export default [
   {
-        icon: '💾',
-    title: 'Project',
-    path: 'https://www.aradechoco.com/',
-    source: 'https://www.aradechoco.com/',
-    description: 'coming soon...',
+        icon: '🔍',
+    title: 'Panduan SEO',
+    path: 'https://www.aradechoco.com/categories/seo/',
+    source: 'https://www.aradechoco.com/categories/seo/',
+    description: 'Belajar SEO untuk Pemula',
   },
   {
-      icon: '💾',
-    title: 'Project',
-    path: 'https://www.aradechoco.com/',
-    source: 'https://www.aradechoco.com/',
-    description: 'coming soon...',
+      icon: '📌',
+    title: 'Panduan Gatsby',
+    path: 'https://www.aradechoco.com/tags/gatsby/',
+    source: 'https://www.aradechoco.com/tags/gatsby/',
+    description: 'Tutorial blog Gatsby',
   },
 
 ]
